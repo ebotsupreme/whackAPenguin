6986d4164ca82a906ee9a8ab806df154b7dc31aa
Eddie Jung <ejung716@gmail.com>
2021-08-19 09:08:13 -0700

    initial commit.

---
+++ Project14/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Eddie Jung on 8/19/21.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
